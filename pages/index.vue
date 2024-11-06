551bba397eab981d65325dd51ea8984abfc28fac
Neil <Neil10241126@gmail.com>
2024-11-06 10:18:55 +0800

    day2-finish

--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,22 @@
 <script setup lang="ts"></script>
 
 <template>
-  <h1>
-    Page: 首頁
-  </h1>
-  <p>目前路由的路徑 -route.fullPath 為: {{ $route.fullPath }}</p>
+  <h1>Page: index</h1>
+
+  <button type="button" class="btn btn-primary">Primary</button>
+  <button type="button" class="btn btn-secondary">Secondary</button>
+  <button type="button" class="btn btn-success">Success</button>
+  <button type="button" class="btn btn-danger">Danger</button>
+  <button type="button" class="btn btn-warning">Warning</button>
+  <button type="button" class="btn btn-info">Info</button>
+  <button type="button" class="btn btn-light">Light</button>
+  <button type="button" class="btn btn-dark">Dark</button>
+  <button type="button" class="btn btn-link">Link</button>
 </template>
 
-<style scoped></style>
+<style scoped lang="scss">
+h1 {
+  color: map-get($theme-colors, primary);
+  line-height: $line-height-lg;
+}
+</style>
